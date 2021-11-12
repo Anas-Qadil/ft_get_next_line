ca3ef055c0d22e0718bb9dd62498b74bcafe239e
Anas Qadil <aqadil@e2r2p2.1337.ma>
2021-11-12 17:15:49 +0100

    updating

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: aqadil <aqadil@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 22:05:48 by aqadil            #+#    #+#             */
-/*   Updated: 2021/11/10 22:09:01 by aqadil           ###   ########.fr       */
+/*   Updated: 2021/11/12 03:06:38 by aqadil           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,16 @@
 # include <unistd.h>
 # include <fcntl.h>
 
-char    *get_next_line(int fd);
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 50
+# endif
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(char *s);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *left_str, char *buff);
+char    *remove_readed_line(char *str);
+char    *get_line(char *str);
+char	*read_the_buffer(char *str, int fd);
 
 #endif
